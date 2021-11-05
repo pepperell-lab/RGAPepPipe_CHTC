26487b6bac7afe2af4d588fae80d8e7a44da1b7a
Madison <youngblom@wisc.edu>
2021-11-05 11:01:30 -0500

    added index step

--- picard.sh
+++ picard.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 java -Xmx2g -jar /opt/PepPrograms/picard-tools-2.26.4/picard.jar MarkDuplicates I=$1.sort.bam O=$1.dedup.bam M=$1.metrics REMOVE_DUPLICATES=true AS=true VALIDATION_STRINGENCY=SILENT
+
 java -Xmx2g -jar /opt/PepPrograms/picard-tools-2.26.4/picard.jar AddOrReplaceReadGroups I=$1.dedup.bam O=$1.ready.bam RGID=$1 RGLB=$2 RGPL=$3 RGPU=dummy-barcode RGSM=$4 VALIDATION_STRINGENCY=SILENT SORT_ORDER=coordinate CREATE_INDEX=true
+
+/opt/PepPrograms/samtools-1.11/bin/samtools index -O $1.ready.bai $1.ready.bam
