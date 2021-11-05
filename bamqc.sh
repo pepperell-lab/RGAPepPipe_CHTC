53d7bf7af1489557bde188c3d47cc1f24d38b42b
Madison <youngblom@wisc.edu>
2021-11-05 10:41:42 -0500

    updated versions

--- bamqc.sh
+++ bamqc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-/opt/PepPrograms/qualimap_v2.2.1/qualimap bamqc -bam $1.realn.bam -outdir $1_bamqc
+/opt/PepPrograms/qualimap_v2.2.1/qualimap bamqc -bam $1.ready.bam -outdir $1_bamqc
 
 tar -zcvf $1.bamqc.tar.gz $1_bamqc/
--- vcf_pilon.sh
+++ vcf_pilon.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /opt/PepPrograms/pilon-1.16.jar --genome $2 --frags $1.realn.bam --output $1_pilon --variant --mindepth 10 --minmq 40 --minqual 20
+java -jar /opt/PepPrograms/pilon-1.24.jar --genome $2 --frags $1.ready.bam --output $1_pilon --variant --mindepth 10 --minmq 40 --minqual 20
