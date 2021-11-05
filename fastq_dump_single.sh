f21fea58894a9ca0888f76e3db8b6c3f8f25f20a
Madison <youngblom@wisc.edu>
2021-11-05 10:43:24 -0500

    moved/removed unused files

--- fastq_dump_single.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-/opt/PepPrograms/sratoolkit.2.8.2-1-ubuntu64/bin/fastq-dump.2.8.2 --split-files $*
-
--- fastqc_postcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-file1=`find $1 -type f -print`
-
-if [ -n "$file1" ]; then #n flag means if string is NOT null -z means IS null
-    exit 1
-fi
