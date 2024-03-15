a1ef0c5cffddebefb8a9429e7cdc92bc6ffb6769
Madeline Topf <48026688+topfm@users.noreply.github.com>
2024-03-14 21:53:25 -0500

    Update trimmomatic.sh

--- trimmomatic.sh
+++ trimmomatic.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-java -jar /opt/PepPrograms/Trimmomatic-0.39/trimmomatic-0.39.jar PE $1_1.fastq.gz $1_2.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:/opt/PepPrograms/bbmap/resources/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50 
+trimmomatic PE $1_1.fastq.gz $1_2.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:/opt/PepPrograms/bbmap/resources/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
