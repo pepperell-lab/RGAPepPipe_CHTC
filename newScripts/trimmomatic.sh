892efbeba5a972dbe9ca1b2e7f6d3ded99eb833b
Madeline Topf <48026688+topfm@users.noreply.github.com>
2024-03-14 21:54:11 -0500

    Delete newScripts directory

--- newScripts/trimmomatic.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-trimmomatic PE $1_1.fastq.gz $1_2.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:/opt/PepPrograms/bbmap/resources/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
