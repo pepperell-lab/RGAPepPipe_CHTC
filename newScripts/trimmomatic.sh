86ab683cb29515bdcb7b15382787082820568a95
Madeline Topf <48026688+topfm@users.noreply.github.com>
2024-03-14 21:39:40 -0500

    Create trimmomatic.sh for chtc

---
+++ newScripts/trimmomatic.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+trimmomatic PE $1_1.fastq.gz $1_2.fastq.gz -baseout $1.fq.gz -threads 4 ILLUMINACLIP:/opt/PepPrograms/bbmap/resources/adapters.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:50
