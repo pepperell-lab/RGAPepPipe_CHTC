978630986672c1772386fc9ddbbd237a5b0af77e
mtopf <mtopf@ap2002.chtc.wisc.edu>
2024-04-21 22:07:33 -0500

    removing obsolete files

--- picard.sh
+++ picard.sh
@@ -13,3 +13,6 @@ conda install picard=2.26.4
 picard MarkDuplicates -I $1.sort.bam -O $1.dedup.bam -M $1.metrics -REMOVE_DUPLICATES true -AS true -VALIDATION_STRINGENCY SILENT
 
 picard AddOrReplaceReadGroups -I $1.dedup.bam -O $1.ready.bam -RGID $1 -RGLB $2 -RGPL $3 -RGPU dummy-barcode -RGSM $4 -VALIDATION_STRINGENCY SILENT -SORT_ORDER coordinate -CREATE_INDEX true
+
+##remove intermediate files
+rm $1.dedup.bam
